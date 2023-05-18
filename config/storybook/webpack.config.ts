7ee0867a364e8bb8e25a19e10eca889e61a3f21a
dzehil02 <dzehil02@gmail.com>
2023-05-18 22:32:54 +0200

    Without screenshot tests

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -14,7 +14,7 @@ export default ({ config }: {config: webpack.Configuration}) => {
     config.resolve.extensions.push('.ts', '.tsx');
 
 
-    config.module.rules = config.module.rules.map((rule: webpack.RuleSetRule) => {
+    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
         }
