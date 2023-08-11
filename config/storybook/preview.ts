ba196d79f0fc8c84850098c1458f4d6323d62a13
dzehil02 <dzehil02@gmail.com>
2023-08-11 11:23:19 +0200

    Add storybook-addon-themes to switch themes

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from "@storybook/react";
 import {StyleDecorator} from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator'
-import {ThemeDecorator} from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator'
+// import {ThemeDecorator} from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator'
 import {RouterDecorator} from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator'
 import {SuspenseDecorator} from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator'
 import {Theme} from '../../src/shared/const/theme'
@@ -14,8 +14,16 @@ const preview: Preview = {
                 date: /Date$/,
             },
         },
+        themes: {
+            default: 'light',
+            list: [
+                { name: 'light', class: ['app', Theme.LIGH], color: '#ffffff' },
+                { name: 'dark', class: ['app', Theme.DARK], color: '#000000' },
+                { name: 'blue', class: ['app', Theme.BLUE], color: '#3b5998' }
+            ],
+        },
     },
-    decorators: [StyleDecorator, ThemeDecorator(Theme.LIGH), RouterDecorator, SuspenseDecorator],
+    decorators: [StyleDecorator, /*ThemeDecorator(Theme.LIGH),*/ RouterDecorator, SuspenseDecorator],
 };
 
 export default preview;
