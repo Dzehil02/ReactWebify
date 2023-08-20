f1e680d8c38982299ed92d05a5c47ef062065e7a
dzehil02 <dzehil02@gmail.com>
2023-08-20 13:32:58 +0200

    Fix rating for ProfilePage

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -6,6 +6,9 @@ import { useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { ProfileRating } from '@/features/profileRating';
 import { Page } from '@/widgets/Page';
+import { getUserAuthData } from '@/entities/User';
+import { getProfileData } from '@/features/editableProfileCard';
+import { useSelector } from 'react-redux';
 
 interface ProfilePageProps {
     className?: string;
@@ -14,6 +17,9 @@ interface ProfilePageProps {
 const ProfilePage: FC<ProfilePageProps> = ({ className }) => {
     const {t} = useTranslation('profile')
     const {id} = useParams<{id: string}>();
+    const authData = useSelector(getUserAuthData);
+    const profileData = useSelector(getProfileData);
+    const canRate = authData?.id !== profileData?.id;
 
     if (!id && __PROJECT__ !== 'storybook') {
         return (
@@ -27,7 +33,7 @@ const ProfilePage: FC<ProfilePageProps> = ({ className }) => {
         <Page data-testid='ProfilePage' className={classNames('', {}, [className])}>
             <VStack max gap='16'>
                 <EditableProfileCard id={id} />
-                <ProfileRating profileId={id} />
+                {canRate && (<ProfileRating profileId={id} />)}
             </VStack>
         </Page>
     )
