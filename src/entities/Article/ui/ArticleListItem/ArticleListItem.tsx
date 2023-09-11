fdd927e10008ff9005b5f704a573ebafb77efea4
dzehil02 <dzehil02@gmail.com>
2023-09-11 17:59:08 +0200

    Redesign ArticleListItem Big view

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -1,22 +1,11 @@
 import { HTMLAttributeAnchorTarget, memo } from 'react';
-import { useTranslation } from 'react-i18next';
-import { classNames } from '@/shared/lib/classNames/classNames';
-import cls from './ArticleListItem.module.scss';
-import { Article, ArticleTextBlock } from '../../model/types/article';
-import { Text } from '@/shared/ui/deprecated/Text';
-import EyeIcon from '@/shared/assets/icons/eye.svg';
-import { Card } from '@/shared/ui/deprecated/Card';
-import { Avatar } from '@/shared/ui/deprecated/Avatar';
-import { Button, ButtonTheme } from '@/shared/ui/deprecated/Button';
-import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
-import { getRouteArticleDetails } from '@/shared/const/router';
-import { AppLink } from '@/shared/ui/deprecated/AppLink';
-import { ArticleBlockType, ArticleView } from '../../model/consts/consts';
-import { AppImage } from '@/shared/ui/redesigned/AppImage';
-import { Skeleton } from '@/shared/ui/deprecated/Skeleton';
-import NotFoundImg from '@/shared/assets/icons/not_found_img.svg';
+import { Article } from '../../model/types/article';
+import { ArticleView } from '../../model/consts/consts';
+import { ArticleListItemDeprecated } from './ArticleListItemDeprecated/ArticleListItemDeprecated';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { ArticleListItemRedesigned } from './ArticleListItemRedesigned/ArticleListItemRedesigned';
 
-interface ArticleListItemProps {
+export interface ArticleListItemProps {
     className?: string;
     article: Article;
     view: ArticleView;
@@ -24,99 +13,11 @@ interface ArticleListItemProps {
 }
 
 export const ArticleListItem = memo((props: ArticleListItemProps) => {
-    const { className, article, view, target } = props;
-    const { t } = useTranslation('article-details');
-
-    const types = <Text text={article.type.join(', ')} className={cls.types} />;
-    const views = (
-        <>
-            <Text text={String(article.views)} className={cls.views} />
-            <EyeIcon className={cls.icon} />
-        </>
-    );
-
-    if (view === ArticleView.BIG) {
-        const textBlock = article.blocks.find(
-            (block) => block.type === ArticleBlockType.TEXT,
-        ) as ArticleTextBlock;
-
-        return (
-            <div
-                data-testid="ArticleListItem"
-                className={classNames(cls.ArticleListItem, {}, [
-                    className,
-                    cls[view],
-                ])}
-            >
-                <Card>
-                    <div className={cls.header}>
-                        <Avatar size={30} src={article.user.avatar} />
-                        <Text
-                            text={article.user.username}
-                            className={cls.username}
-                        />
-                        <Text text={article.createdAt} className={cls.date} />
-                    </div>
-                    <Text title={article.title} className={cls.title} />
-                    {types}
-                    <AppImage
-                        fallback={<Skeleton width={'100%'} height={250} />}
-                        errorFallback={
-                            <NotFoundImg width={'100%'} height={200} />
-                        }
-                        src={article.img}
-                        className={cls.img}
-                        alt={article.title}
-                    />
-                    {textBlock && (
-                        <ArticleTextBlockComponent
-                            block={textBlock}
-                            className={cls.textBlock}
-                        />
-                    )}
-                    <div className={cls.footer}>
-                        <AppLink
-                            to={getRouteArticleDetails(article.id)}
-                            target={target}
-                        >
-                            <Button theme={ButtonTheme.OUTLINE}>
-                                {t('Read more')}
-                            </Button>
-                        </AppLink>
-                        {views}
-                    </div>
-                </Card>
-            </div>
-        );
-    }
-
     return (
-        <AppLink
-            data-testid="ArticleListItem"
-            to={getRouteArticleDetails(article.id)}
-            target={target}
-            className={classNames(cls.ArticleListItem, {}, [
-                className,
-                cls[view],
-            ])}
-        >
-            <Card>
-                <div className={cls.imageWrapper}>
-                    <AppImage
-                        fallback={<Skeleton width={200} height={200} />}
-                        alt={article.title}
-                        src={article.img}
-                        className={cls.img}
-                        errorFallback={<NotFoundImg width={200} height={200} />}
-                    />
-                    <Text text={article.createdAt} className={cls.date} />
-                </div>
-                <div className={cls.infoWrapper}>
-                    {types}
-                    {views}
-                </div>
-                <Text text={article.title} className={cls.title} />
-            </Card>
-        </AppLink>
+        <ToggleFeatures
+            feature="isAppRedesigned"
+            off={<ArticleListItemDeprecated {...props} />}
+            on={<ArticleListItemRedesigned {...props} />}
+        />
     );
 });
---
+++ src/entities/Article/ui/ArticleListItem/ArticleListItemRedesigned/ArticleListItemRedesigned.tsx
@@ -0,0 +1,118 @@
+import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from '../ArticleListItem.module.scss';
+import { ArticleTextBlock } from '../../../model/types/article';
+import { Text } from '@/shared/ui/redesigned/Text';
+import EyeIcon from '@/shared/assets/icons/eyeNew.svg';
+import { Card } from '@/shared/ui/redesigned/Card';
+import { Avatar } from '@/shared/ui/redesigned/Avatar';
+import { Button } from '@/shared/ui/redesigned/Button';
+import { getRouteArticleDetails } from '@/shared/const/router';
+import { AppLink } from '@/shared/ui/redesigned/AppLink';
+import { ArticleBlockType, ArticleView } from '../../../model/consts/consts';
+import { AppImage } from '@/shared/ui/redesigned/AppImage';
+import { Skeleton } from '@/shared/ui/redesigned/Skeleton';
+import NotFoundImg from '@/shared/assets/icons/not_found_img.svg';
+import { ArticleListItemProps } from '../ArticleListItem';
+import { HStack, VStack } from '@/shared/ui/redesigned/Stack';
+import { Icon } from '@/shared/ui/redesigned/Icon';
+
+export const ArticleListItemRedesigned = memo((props: ArticleListItemProps) => {
+    const { className, article, view, target } = props;
+    const { t } = useTranslation('article-details');
+
+    const types = <Text text={article.type.join(', ')} className={cls.types} />;
+    const views = (
+        <HStack gap={'8'}>
+            <Icon Svg={EyeIcon} />
+            <Text text={String(article.views)} className={cls.views} />
+        </HStack>
+    );
+
+    if (view === ArticleView.BIG) {
+        const textBlock = article.blocks.find(
+            (block) => block.type === ArticleBlockType.TEXT,
+        ) as ArticleTextBlock;
+
+        return (
+            <Card
+                padding="24"
+                max
+                data-testid="ArticleListItem"
+                className={classNames(cls.ArticleListItem, {}, [
+                    className,
+                    cls[view],
+                ])}
+            >
+                <VStack max gap={'16'}>
+                    <HStack max gap={'8'} justify="between">
+                        <HStack max gap={'8'}>
+                            <Avatar size={32} src={article.user.avatar} />
+                            <Text bold text={article.user.username} />
+                            <Text text={article.createdAt} />
+                        </HStack>
+                        {types}
+                    </HStack>
+                    <Text bold title={article.title} />
+                    <Text title={article.subtitle} size="s" />
+                    <AppImage
+                        fallback={<Skeleton width={'100%'} height={250} />}
+                        errorFallback={
+                            <NotFoundImg width={'100%'} height={200} />
+                        }
+                        src={article.img}
+                        className={cls.imgRedesigned}
+                        alt={article.title}
+                    />
+                    {textBlock?.paragraphs && (
+                        <Text
+                            className={cls.textBlockRedesigned}
+                            text={textBlock.paragraphs.slice(0, 2).join(' ')}
+                        />
+                    )}
+                    <HStack max justify={'between'}>
+                        <AppLink
+                            to={getRouteArticleDetails(article.id)}
+                            target={target}
+                        >
+                            <Button variant="outline">{t('Read more')}</Button>
+                        </AppLink>
+                        {views}
+                    </HStack>
+                </VStack>
+                <div className={cls.footer}></div>
+            </Card>
+        );
+    }
+
+    return (
+        <AppLink
+            data-testid="ArticleListItem"
+            to={getRouteArticleDetails(article.id)}
+            target={target}
+            className={classNames(cls.ArticleListItem, {}, [
+                className,
+                cls[view],
+            ])}
+        >
+            <Card>
+                <div className={cls.imageWrapper}>
+                    <AppImage
+                        fallback={<Skeleton width={200} height={200} />}
+                        alt={article.title}
+                        src={article.img}
+                        className={cls.img}
+                        errorFallback={<NotFoundImg width={200} height={200} />}
+                    />
+                    <Text text={article.createdAt} className={cls.date} />
+                </div>
+                <div className={cls.infoWrapper}>
+                    {types}
+                    {views}
+                </div>
+                <Text text={article.title} className={cls.title} />
+            </Card>
+        </AppLink>
+    );
+});
